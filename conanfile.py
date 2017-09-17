fe68a29ff05ccbbea71eb495d6ba2ab452a0db0b
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:34:32 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,30 +2,30 @@ from conans import ConanFile, tools, os
 
 class BoostSignals2Conan(ConanFile):
     name = "Boost.Signals2"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-signals2"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["signals2"]
-    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Bind/1.64.0@bincrafters/testing", \
-                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Function/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Multi_Index/1.64.0@bincrafters/testing", \
-                      "Boost.Optional/1.64.0@bincrafters/testing", \
-                      "Boost.Parameter/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
-                      "Boost.Tuple/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
-                      "Boost.Variant/1.64.0@bincrafters/testing"
+    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Bind/1.65.1@bincrafters/testing", \
+                      "Boost.Concept_Check/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Function/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Multi_Index/1.65.1@bincrafters/testing", \
+                      "Boost.Optional/1.65.1@bincrafters/testing", \
+                      "Boost.Parameter/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Tuple/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+                      "Boost.Variant/1.65.1@bincrafters/testing"
 
                       #assert1 bind3 config0 core2 function5 iterator5 mpl5 multi_index12 optional5 parameter10 predef0 preprocessor0 smart_ptr4 throw_exception2 tuple4 type_traits3 variant9
                       
--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-conan upload --all --remote bincrafters Boost.Signals2/1.64.0@bincrafters/testing
+conan upload --all --remote bincrafters Boost.Signals2/1.65.1@bincrafters/testing
