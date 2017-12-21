fa8d8a7dc645a8c79624cf2d5b9eb7d524b6ce1e
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:35 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,27 +3,27 @@ from conans import ConanFile
 
 class BoostSignals2Conan(ConanFile):
     name = "Boost.Signals2"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Bind/1.65.1@bincrafters/testing", \
-        "Boost.Concept_Check/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Function/1.65.1@bincrafters/testing", \
-        "Boost.Iterator/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Multi_Index/1.65.1@bincrafters/testing", \
-        "Boost.Optional/1.65.1@bincrafters/testing", \
-        "Boost.Parameter/1.65.1@bincrafters/testing", \
-        "Boost.Predef/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Tuple/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Variant/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Bind/1.66.0@bincrafters/testing", \
+        "Boost.Concept_Check/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Function/1.66.0@bincrafters/testing", \
+        "Boost.Iterator/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Multi_Index/1.66.0@bincrafters/testing", \
+        "Boost.Optional/1.66.0@bincrafters/testing", \
+        "Boost.Parameter/1.66.0@bincrafters/testing", \
+        "Boost.Predef/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Tuple/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Variant/1.66.0@bincrafters/testing"
 
     lib_short_names = ["signals2"]
     is_header_only = True
@@ -31,9 +31,9 @@ class BoostSignals2Conan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-signals2"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
