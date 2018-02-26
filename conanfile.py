c89a220c38b30571fe74a180390a0bb1cc3586d7
Jerry W <jerrywiltse@gmail.com>
2018-02-26 09:05:58 -0500

    set deps to stable

--- conanfile.py
+++ conanfile.py
@@ -17,25 +17,25 @@ class BoostSignals2Conan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_assert/1.65.1@bincrafters/testing",
-        "boost_bind/1.65.1@bincrafters/testing",
-        "boost_concept_check/1.65.1@bincrafters/testing",
-        "boost_config/1.65.1@bincrafters/testing",
-        "boost_core/1.65.1@bincrafters/testing",
-        "boost_function/1.65.1@bincrafters/testing",
-        "boost_iterator/1.65.1@bincrafters/testing",
-        "boost_mpl/1.65.1@bincrafters/testing",
-        "boost_multi_index/1.65.1@bincrafters/testing",
-        "boost_optional/1.65.1@bincrafters/testing",
-        "boost_parameter/1.65.1@bincrafters/testing",
-        "boost_predef/1.65.1@bincrafters/testing",
-        "boost_preprocessor/1.65.1@bincrafters/testing",
-        "boost_smart_ptr/1.65.1@bincrafters/testing",
-        "boost_throw_exception/1.65.1@bincrafters/testing",
-        "boost_tuple/1.65.1@bincrafters/testing",
-        "boost_type_traits/1.65.1@bincrafters/testing",
-        "boost_variant/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_assert/1.65.1@bincrafters/stable",
+        "boost_bind/1.65.1@bincrafters/stable",
+        "boost_concept_check/1.65.1@bincrafters/stable",
+        "boost_config/1.65.1@bincrafters/stable",
+        "boost_core/1.65.1@bincrafters/stable",
+        "boost_function/1.65.1@bincrafters/stable",
+        "boost_iterator/1.65.1@bincrafters/stable",
+        "boost_mpl/1.65.1@bincrafters/stable",
+        "boost_multi_index/1.65.1@bincrafters/stable",
+        "boost_optional/1.65.1@bincrafters/stable",
+        "boost_parameter/1.65.1@bincrafters/stable",
+        "boost_predef/1.65.1@bincrafters/stable",
+        "boost_preprocessor/1.65.1@bincrafters/stable",
+        "boost_smart_ptr/1.65.1@bincrafters/stable",
+        "boost_throw_exception/1.65.1@bincrafters/stable",
+        "boost_tuple/1.65.1@bincrafters/stable",
+        "boost_type_traits/1.65.1@bincrafters/stable",
+        "boost_variant/1.65.1@bincrafters/stable"
     )
 
     # BEGIN
@@ -43,7 +43,7 @@ class BoostSignals2Conan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
