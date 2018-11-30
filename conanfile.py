516a40f5ed001a96db21e48f18e0b649ed42e525
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostSignals2Conan(base.BoostBaseConan):
     name = "boost_signals2"
@@ -30,5 +30,3 @@ class BoostSignals2Conan(base.BoostBaseConan):
         "boost_type_traits",
         "boost_variant"
     ]
-
-
