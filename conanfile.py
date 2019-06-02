1b3a6a82483f549c824a5cc1b02aeb368ad03def
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,30 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostSignals2Conan(base.BoostBaseConan):
     name = "boost_signals2"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_signals2"
-    lib_short_names = ["signals2"]
-    header_only_libs = ["signals2"]
-    b2_requires = [
-        "boost_assert",
-        "boost_bind",
-        "boost_config",
-        "boost_core",
-        "boost_function",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_multi_index",
-        "boost_optional",
-        "boost_parameter",
-        "boost_predef",
-        "boost_preprocessor",
-        "boost_smart_ptr",
-        "boost_throw_exception",
-        "boost_tuple",
-        "boost_type_traits",
-        "boost_variant"
-    ]
+    version = "1.70.0"
